9169900041981581395dc1db78c7d25a256e6ce2
Patrick <patrick.lpz87@gmail.com>
2015-11-12 08:53:36 +0100

    Initial commit.

---
+++ ResearchProject_M3/src/main/java/de/arnold/m3/Main.java
@@ -0,0 +1,20 @@
+package de.arnold.m3;
+
+
+/**
+ * This class is used to launch the program.
+ * 
+ * <br><br><b>Initial date:</b> 12.11.2015
+ * @author Arnold
+ */
+public class Main {
+
+	/**
+	 * This method is used to launch the program.
+	 * @param args Optional parameters.
+	 */
+	public static void main( String[] args) {
+		
+	}
+	
+}
